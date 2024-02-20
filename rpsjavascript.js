4a0c88e76c95975e67064cdac321b5b82a9bb6c4
Operon5 <allan.moloney@hotmail.com>
2024-02-20 16:38:35 -0500

    added working function to play single rock paper scissors round, added player and computer win counters

--- rpsjavascript.js
+++ rpsjavascript.js
@@ -1,25 +1,58 @@
-let options = ['Rock', 'Paper', 'Scissors'];
+let options = ['rock', 'paper', 'scissors'];
 
 function getComputerChoice() {
     let decision = (options[Math.floor(Math.random() * options.length)]);
     return decision;
 }
 
-function playerSelection() {
+function playerDecision() {
     let playerEntry = prompt('Choose Rock, Paper or Scissors.').toLowerCase();
     
     if (playerEntry === 'rock' || playerEntry === 'paper' || playerEntry === 'scissors') {
-        let playerDecision = playerEntry;
-        return playerDecision;
+        let playerChoice = playerEntry;
+        return playerChoice;
     }
     else {
         alert('Please choose Rock, Paper or Scissors.');
-        playerSelection();
+        playerDecision();
     }
 }
 
-
 let computerSelection = getComputerChoice();
+let playerSelection = playerDecision();
+let playerCounter = 0;
+let computerCounter = 0;
 
-playerSelection();
+function playRound() {
+    if (playerSelection === computerSelection) {
+        alert("It's a draw!");
+    }
+    else if (playerSelection === 'rock' && computerSelection === 'paper') {
+        alert('You lose...');
+        ++computerCounter;
+    }
+    else if (playerSelection === 'rock' && computerSelection === 'scissors') {
+        alert('You win!');
+        ++playerCounter;
+    }
+    else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+        alert('You lose...');
+        ++computerCounter;
+    }
+    else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+        alert('You win!');
+        ++playerCounter;
+    }
+    else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+        alert('You lose...');
+        ++computerCounter;
+    }
+    else if (playerSelection === 'paper' && computerSelection === 'rock') {
+        alert('You win!');
+        ++playerCounter;
+    }
+}
+
+playRound();
+
 console.log(computerSelection);
